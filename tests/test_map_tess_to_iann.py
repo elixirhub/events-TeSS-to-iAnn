8ed8aebdb26a9df4115a05a5adff442fd10b0a9d
ficolo <fico89@gmail.com>
2016-10-17 14:25:53 +0100

    Adding some unit tests

--- tests/test_map_tess_to_iann.py
+++ tests/test_map_tess_to_iann.py
@@ -1,5 +1,6 @@
 import tess_to_iann
 from mock import patch
+import test_conf as conf
 
 
 def test_empty_entry():
@@ -13,14 +14,14 @@ def test_none_entry():
 
 
 def test_some_entry():
-    print tess_to_iann.conf.__name__
-    description = 'This course will provide an introduction to next generation sequencing (NGS) platforms'
-    test_entry = dict(title='Hi there!', start='2016-10-20T09:00:00.000Z', end='2016-10-19T10:30:00.000Z',
-                      url='https://www.statslife.org.uk/events/eventdetail/751/12/applying-for-certification',
-                      venue='The Royal Statistical Society', organizer='University of Leicester',
-                      country='United Kingdom', description=description)
-    test_output = dict(name='Hi there!', start_date='2016-10-20T09:00:00.000Z', end_date='2016-10-19T10:30:00.000Z',
-                       website='https://www.statslife.org.uk/events/eventdetail/751/12/applying-for-certification',
-                       location='The Royal Statistical Society', host='University of Leicester',
-                       country='United Kingdom', description=description)
-    assert tess_to_iann.map_tess_to_iann(test_entry) == test_output
+    with patch('tess_to_iann.conf', new=conf):
+        description = 'This course will provide an introduction to next generation sequencing (NGS) platforms'
+        test_entry = dict(title='Hi there!', start='2016-10-20T09:00:00.000Z', end='2016-10-19T10:30:00.000Z',
+                          url='https://www.statslife.org.uk/events/eventdetail/751/12/applying-for-certification',
+                          venue='The Royal Statistical Society', organizer='University of Leicester',
+                          country='United Kingdom', description=description)
+        test_output = dict(name='Hi there!', start_date='2016-10-20T09:00:00.000Z', end_date='2016-10-19T10:30:00.000Z',
+                           website='https://www.statslife.org.uk/events/eventdetail/751/12/applying-for-certification',
+                           location='The Royal Statistical Society', host='University of Leicester',
+                           country='United Kingdom', description=description)
+        assert tess_to_iann.map_tess_to_iann(test_entry) == test_output
